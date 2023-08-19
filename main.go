71fa502c19b72c80e55ae680d76593afe8f09593
Muhammed-Awwal <awwalcodestar@gmail.com>
2023-08-19 05:30:03 +0100

    Defined getMovies, getMovie and deletMovie handler functions

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"encoding/json"
 	"strconv"
 	"math/rand"
 	"github.com/gorilla/mux"
@@ -16,13 +17,58 @@ type Movie struct {
 	Director *Director `json: "director"`
 }
 
+var movies []Movie
+
 type Director struct {
 	Firstname string `json: "firstname"`
 	Lastname string `json: "lastname"`
 }
 
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+
+}
+
+func getMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+	for _, item := range movies {
+		if item.Id == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.Id == params["id"] {
+			movies = append(movies[:index], movies[index + 1:]...)
+			break
+		}
+	}
+}
+
 func main() {
+	r := mux.NewRouter()
+
+	movies = append(movies, Movie {Id: "1", Isbn: "435-234-098", Title: "The Good Doctor", Director: &Director{Firstname: "Steve", Lastname: "Lee"}})
+	movies = append(movies, Movie {Id: "2", Isbn: "477-274-098", Title: "One of Us is Lying", Director: &Director{Firstname: "Robert", Lastname: "Coulson"}})
 	
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Println("Starting server at 8000......")
+	log.Fatal(http.ListenAndServe(":8000", r))
+
+
 
 }
 
